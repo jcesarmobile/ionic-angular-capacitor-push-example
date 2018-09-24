4528f621bdcf4dbff585bd4c9245bf1715c92085
jcesarmobile <jcesarmobile@gmail.com>
2018-09-24 20:40:21 +0200

    Add Push code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Plugins, PushNotification, PushNotificationToken } from '@capacitor/core';
+
+const { PushNotifications } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  ngOnInit() {
+    console.log('ng init');
+    PushNotifications.register();
+    PushNotifications.addListener('registration', (token: PushNotificationToken) => {
+      console.log('token ' + token.value);
+    });
+    PushNotifications.addListener('registrationError', (error: any) => {
+      console.log('error on register ' + JSON.stringify(error));
+    });
+    PushNotifications.addListener('pushNotificationReceived', (notification: PushNotification) => {
+      console.log('notification ' + JSON.stringify(notification));
+    });
+  }
 }
